0fc3921c8439645740ed8876c2af3056c8c331ef
Jeremy C. Swain <jeremycswain@icloud.com>
2016-06-16 21:07:27 -0500

    added user model/rename truckprofile model

--- models/truck_profile.js
+++ models/truck_profile.js
@@ -6,12 +6,12 @@ var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 // Schema
-var truckUserSchema = new mongoose.Schema({
+var truckProfileSchema = new mongoose.Schema({
 	truck_name: String, 
-	cuisine: String, 
 	contact_info: String,
+	cuisine: String, 
 	website_url: String 
 });
 
 // Return Model
-module.exports = restful.model('TruckUser', truckUserSchema);
+module.exports = restful.model('TruckProfile', truckProfileSchema);
